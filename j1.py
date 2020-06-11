48274e6faba2b9c99f16f5445aaf6b852f2a4c02
LuanaAbramoff <62798202+LuanaAbramoff@users.noreply.github.com>
2020-06-11 18:28:03 -0300

    v18 animacao de inicio tenativa

--- j1.py
+++ j1.py
@@ -81,6 +81,24 @@ for i in range(36):
     listaanimacao.append(img)
 assets['animacao para passar de fase']=listaanimacao
 
+#animacaodeinicio
+listainicio = []
+for i in range(158):
+    filename = 'assets/imagens/animacaodeinicio ({}).png'.format(i)
+    img = pygame.image.load(filename).convert()
+    img = pygame.transform.scale(img,(603,601))
+    listainicio.append(img)
+assets['animacao de inicio']=listainicio
+
+#animacaoespaco
+listaespaco = []
+for i in range(8):
+    filename = 'assets/imagens/aperteespaço ({}).png'.format(i)
+    img = pygame.image.load(filename).convert()
+    img =  pygame.transform.scale(img,(603,601))
+    listaespaco.append(img)
+assets['animacao espaco']=listaespaco
+
 #funcao que sorteia a seguencia das teclas
 def sorteiasequencia(x):
     i=0
@@ -217,6 +235,53 @@ class Animacao (pygame.sprite.Sprite):
             elif self.image == assets['azul'] or self.image == assets['desligado verde']:
                 assets['som da tecla azul'].play()
 
+class AnimacaoInicio(pygame.sprite.Sprite):
+    def __init__(self, assets):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        # Armazena a animação de explosão
+        self.animacaodeinicio = assets['animacao de inicio']
+
+        # Inicia o processo de animação colocando a primeira imagem na tela.
+        self.frame = 0  # Armazena o índice atual na animação
+        self.image = self.animacaodeinicio[self.frame]  # Pega a primeira imagem
+        self.rect = self.image.get_rect()
+        self.rect.x = ximagem
+        self.rect.y = yimagem
+
+        # Guarda o tick da primeira imagem, ou seja, o momento em que a imagem foi mostrada
+        self.last_update = pygame.time.get_ticks()
+
+        # Controle de ticks de animação: troca de imagem a cada self.frame_ticks milissegundos.
+        # Quando pygame.time.get_ticks() - self.last_update > self.frame_ticks a
+        # próxima imagem da animação será mostrada
+        self.frame_ticks = 50
+
+    def update(self):
+        # Verifica o tick atual.
+        now = pygame.time.get_ticks()
+        # Verifica quantos ticks se passaram desde a ultima mudança de frame.
+        elapsed_ticks = now - self.last_update
+
+        # Se já está na hora de mudar de imagem...
+        if elapsed_ticks > self.frame_ticks:
+            # Marca o tick da nova imagem.
+            self.last_update = now
+
+            # Avança um quadro.
+            self.frame += 1
+
+            # Verifica se já chegou no final da animação.
+            if self.frame == len(self.animacaodeinicio):
+                # Se sim, acaba com a animação
+                self.kill()
+            else:
+                # Se ainda não chegou ao fim da animacao, troca de imagem.
+                self.image = self.animacaodeinicio[self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.x = ximagem
+                self.rect.y = yimagem
 class Animacaopassadefase(pygame.sprite.Sprite):
     def __init__(self, assets):
         # Construtor da classe mãe (Sprite).
@@ -265,6 +330,53 @@ class Animacaopassadefase(pygame.sprite.Sprite):
                 self.rect.x = ximagem
                 self.rect.y = yimagem
 
+class AnimacaoEspaco(pygame.sprite.Sprite):
+    def __init__(self, assets):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        # Armazena a animação de explosão
+        self.animacaoespaco = assets['animacao espaco']
+
+        # Inicia o processo de animação colocando a primeira imagem na tela.
+        self.frame = 0  # Armazena o índice atual na animação
+        self.image = self.animacaoespaco[self.frame]  # Pega a primeira imagem
+        self.rect = self.image.get_rect()
+        self.rect.x = ximagem
+        self.rect.y = yimagem
+
+        # Guarda o tick da primeira imagem, ou seja, o momento em que a imagem foi mostrada
+        self.last_update = pygame.time.get_ticks()
+
+        # Controle de ticks de animação: troca de imagem a cada self.frame_ticks milissegundos.
+        # Quando pygame.time.get_ticks() - self.last_update > self.frame_ticks a
+        # próxima imagem da animação será mostrada
+        self.frame_ticks = 50
+
+    def update(self):
+        # Verifica o tick atual.
+        now = pygame.time.get_ticks()
+        # Verifica quantos ticks se passaram desde a ultima mudança de frame.
+        elapsed_ticks = now - self.last_update
+
+        # Se já está na hora de mudar de imagem...
+        if elapsed_ticks > self.frame_ticks:
+            # Marca o tick da nova imagem.
+            self.last_update = now
+
+            # Avança um quadro.
+            self.frame += 1
+
+            # Verifica se já chegou no final da animação.
+            if self.frame == len(self.animacaoespaco):
+                # Se sim, acaba com a animação
+                self.kill()
+            else:
+                # Se ainda não chegou ao fim da animacao, troca de imagem.
+                self.image = self.animacaoespaco[self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.x = ximagem
+                self.rect.y = yimagem
 #cria o grupo dos objetos 
 all_sprites = pygame.sprite.Group()
 
@@ -279,6 +391,20 @@ vidas=3
 seq = sorteiasequencia(nivel) #cria primeira sequencia
 
 botao_atual = 0 #caminha dentro da sequencia criada
+#animacaodeinicio = AnimacaoInicio(assets)
+#all_sprites.add(animacaodeinicio)
+#animacaodeinicio.kill()
+animaespaco = AnimacaoEspaco(assets)
+all_sprites.add(animaespaco)
+for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+        while event.key != pygame.K_SPACE:
+            animaespaco = AnimacaoEspaco(assets)
+            all_sprites.add(animaespaco)
+            game = False
+        if event.key == pygame.K_SPACE:
+            animaespaco.kill()
+            game = True
 animacao = Animacao(assets, seq, fase) #cria uma animacao
 all_sprites.add(animacao) #adiciona animacao
 mudando_fase = False
